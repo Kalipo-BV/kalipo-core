56e59da3fa7e4abbfb8f984dd64a61e7aec0a185
kestervanleeuwen <kester.vanleeuwen@student.hu.nl>
2022-09-28 10:20:00 +0000

    init commit

---
+++ test/unit/modules/blog/assets/post_asset.spec.ts
@@ -0,0 +1,40 @@
+import { PostAsset } from '../../../../../src/app/modules/blog/assets/post_asset';
+
+describe('PostAsset', () => {
+  let transactionAsset: PostAsset;
+
+	beforeEach(() => {
+		transactionAsset = new PostAsset();
+	});
+
+	describe('constructor', () => {
+		it('should have valid id', () => {
+			expect(transactionAsset.id).toEqual(0);
+		});
+
+		it('should have valid name', () => {
+			expect(transactionAsset.name).toEqual('post');
+		});
+
+		it('should have valid schema', () => {
+			expect(transactionAsset.schema).toMatchSnapshot();
+		});
+	});
+
+	describe('validate', () => {
+		describe('schema validation', () => {
+      it.todo('should throw errors for invalid schema');
+      it.todo('should be ok for valid schema');
+    });
+	});
+
+	describe('apply', () => {
+    describe('valid cases', () => {
+      it.todo('should update the state store');
+    });
+
+    describe('invalid cases', () => {
+      it.todo('should throw error');
+    });
+	});
+});
