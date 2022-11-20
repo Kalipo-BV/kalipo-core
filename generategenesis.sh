02d6e691443651cb17e18f789d1f87b6133dbfa5
kestervanleeuwen <kester.vanleeuwen@student.hu.nl>
2022-11-20 13:09:52 +0000

    added script

---
+++ generategenesis.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+printf '%s\n' y | ./bin/run genesis-block:create && 
+tmp=$(mktemp) &&
+jq '.forging.delegates = input' config/default/config.json config/default/forging_info.json > "$tmp" && mv "$tmp" config/default/config.json &&
+jq '.forging += input' config/default/config.json config/default/password.json > "$tmp" && mv "$tmp" config/default/config.json &&
+rm -r ~/.lisk/kalipo-core
